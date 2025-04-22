62beebcf122ef7b8818a4f8fd84c4333406b92d6
davideerusso <rd.96.rd@gmail.com>
2025-04-22 15:14:44 +0200

    es1

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,8 @@
-const names = ['Edoardo', 'Simone', 'Francesco'];
+const names = ["Edoardo", "Simone", "Francesco"];
 
+let name = names.map((stamp) => {
+  return stamp;
+});
+console.log(name);
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'

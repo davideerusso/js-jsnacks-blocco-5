e4c8f9b196c05376787936f5665c671cfdd30b93
davideerusso <rd.96.rd@gmail.com>
2025-04-22 16:50:18 +0200

    es8

--- snack-8/script.js
+++ snack-8/script.js
@@ -1,23 +1,35 @@
 const students = [
   {
     id: 1,
-    name: 'Marco Lanci',
+    name: "Marco Lanci",
     age: 32,
-    class: '3C'
+    class: "3C",
   },
   {
     id: 2,
-    name: 'Mario Banfi',
+    name: "Mario Banfi",
     age: 34,
-    class: '4A'
+    class: "4A",
   },
   {
     id: 3,
-    name: 'Luigi Banzi',
+    name: "Luigi Banzi",
     age: 33,
-    class: '5B'
+    class: "5B",
   },
 ];
+
+const marco = students.filter((student) => {
+  if (student.name === "Marco Lanci") {
+    return `${student.class}`;
+  }
+});
+console.log(marco);
+
+let marcoClass = marco.map((classe) => {
+  return `${classe.class}`;
+});
+console.log(marcoClass);
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
